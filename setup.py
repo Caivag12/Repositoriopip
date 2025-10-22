98be7518170c1563bcb170db9ad44cc14b93debf
Caivag12 <carlosivan291199@gmail.com>
2025-10-22 10:59:43 -0600

    lanzamiento 1er paquete

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Repositoiopip',
+    version='0.1',
+    packages=find_packages(),
+    description='Un repositorio de ejemplo para demostrar la configuración de setup.py',
+    author='Ivan Aguirre',
+    author_email='carlosivan291199@gmail.com',
+    url='https://github.com/Caivag12/Repositoriopip',
+    
+)
